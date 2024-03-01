5f526f8b4c25dcd2b9d0945fbdc4ca6292fbdf08
eriksonJAguiar <erjulioaguiar@gmail.com>
2024-03-01 13:05:31 -0300

    feat: test continual learning

--- continual_learning/continual.py
+++ continual_learning/continual.py
@@ -6,7 +6,7 @@ from avalanche.models import as_multitask
 from avalanche.benchmarks.datasets import FashionMNIST, MNIST
 from avalanche.benchmarks.generators import nc_benchmark, ni_benchmark
 from avalanche.benchmarks.generators import dataset_benchmark
-from avalanche.benchmarks.utils import make_classification_dataset
+from avalanche.benchmarks.utils import make_classification_dataset, as_classification_dataset
 import torch
 import torchvision
 import os
@@ -41,22 +41,22 @@ from utils import utils
 #     print("This task contains", len(current_test_set), "Test examples")
 
 
-# from torchvision.transforms import Compose, ToTensor, Normalize, RandomCrop
-# train_transform = Compose([
-#     RandomCrop(28, padding=4),
-#     ToTensor(),
-#     Normalize((0.1307,), (0.3081,))
-# ])
+from torchvision.transforms import Compose, ToTensor, Normalize, RandomCrop
+train_transform = Compose([
+    RandomCrop(28, padding=4),
+    ToTensor(),
+    Normalize((0.1307,), (0.3081,))
+])
 
 # test_transform = Compose([
 #     ToTensor(),
 #     Normalize((0.1307,), (0.3081,))
 # ])
 
-# mnist_train = MNIST(
-#     '../data/mnist', train=True, download=True, transform=train_transform
-# )
-# print(type(mnist_train))
+mnist_train = MNIST(
+    '../data/mnist', train=True, download=True, transform=train_transform
+)
+print(type(mnist_train.train))
 # mnist_test = MNIST(
 #     '../data/mnist', train=False, download=True, transform=test_transform
 # )
@@ -72,14 +72,14 @@ def run_continual(root_path, csv_path):
         image_size=(12,128),
         is_agumentation=False,
         as_rgb=True,
-        is_stream=True
+        #is_stream=True
     )
     
     train = values["train"]
     test = values["test"]
     num_class = values["num_class"]
     
-    print(train[0])
+    #print(train[0])
     # scenario = ni_benchmark(
     #     train_dataset=train, 
     #     test_dataset=test,
@@ -89,15 +89,9 @@ def run_continual(root_path, csv_path):
     #     balance_experiences=True
     # )
     
-    train_domains = []
-    test_domains = []
-    for tr, te in zip(train, test):
-        train_domains.append(make_classification_dataset(tr))
-        test_domains.append(make_classification_dataset(te))
-    
     scenario = dataset_benchmark(
-        train_datasets=train_domains,
-        test_datasets=test_domains,
+        train_datasets=[train[0]],   
+        test_datasets=[test[0]],
     )
     
     print('Without custom task labels:', scenario.train_stream[1].task_label)
